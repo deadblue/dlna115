6a89f4bd001e56c23b4a8a3de5a018083db5601c
Tomo Kunagisa <root@dead.blue>
2023-08-27 12:10:11 +0800

    Move main code to daemin.Run.

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,50 +2,13 @@ package main
 
 import (
 	"log"
-	"os"
-	"os/signal"
 
 	"github.com/deadblue/dlna115/internal/daemon"
-	"github.com/deadblue/dlna115/internal/mediaserver"
 )
 
 func main() {
-	var err error
-	args := (&daemon.Args{})
-	if err = args.Init(); err != nil {
-		log.Fatal(err)
-	}
-
-	// Handle OS signal
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, os.Interrupt)
-	defer func() {
-		signal.Stop(sigChan)
-		close(sigChan)
-	}()
-
-	// Create media service
-	ms := mediaserver.New(&args.Media)
-	// Start media service
-	if err = ms.Startup(); err != nil {
-		log.Fatal(err)
-	}
-	// ssdp.NotifyDeviceAvailable(ms)
-
-	// Loop
-	for running := true; running; {
-		select {
-		case <-sigChan:
-			log.Printf("Shutdown DLNA media server ...")
-			ms.Shutdown()
-		case err = <-ms.ErrChan():
-			if err != nil {
-				log.Printf("Media server closed with error: %s", err)
-			} else {
-				log.Println("Media server closed normally!")
-			}
-			running = false
-		}
+	if err := daemon.Run(); err != nil {
+		log.Fatalf("DLNA115 exited with error: %s", err)
 	}
 
 }

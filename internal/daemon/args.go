6697049a7089d774d085d1397eae944b2488ac38
Tomo Kunagisa <root@dead.blue>
2023-08-27 22:09:31 +0800

    Use hostname as default server name.

--- internal/daemon/args.go
+++ internal/daemon/args.go
@@ -23,13 +23,16 @@ func (a *Args) Init() (err error) {
 	fs.StringVar(&a.Media.UUID, "media-uuid", "", "The UUID of media server.")
 	fs.BoolVar(&a.Media.UseHLS, "media-hls", true, "Streaming video via HLS.")
 	fs.BoolVar(&a.WithSSDP, "ssdp", false, "Start an SSDP server.")
-	var configFile string
-	fs.StringVar(&configFile, "config", "", "Config file")
-	if err = fs.Parse(os.Args[1:]); err != nil {
-		return
-	}
+	// var configFile string
+	// fs.StringVar(&configFile, "config", "", "Config file")
+	// if err = fs.Parse(os.Args[1:]); err != nil {
+	// 	return
+	// }
 	if a.Media.UUID == "" {
 		a.Media.UUID = uuid.NewString()
+	}
+	if a.Media.Name == "" {
+		a.Media.Name, _ = os.Hostname()
 	}
 	return
 }

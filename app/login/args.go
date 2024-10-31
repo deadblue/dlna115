9b5fa540b8eab72e91eda8ce2a291fa0ff88cc85
Tomo Kunagisa <root@dead.blue>
2024-10-31 23:27:02 +0800

    Support multiple login platform.

--- app/login/args.go
+++ app/login/args.go
@@ -3,30 +3,38 @@ package login
 import (
 	"flag"
 	"fmt"
+
+	"github.com/deadblue/elevengo/option"
 )
 
 func (c *Command) Init(args []string) (err error) {
 	// Parse args
 	fs := flag.NewFlagSet("", flag.ContinueOnError)
 	fs.Usage = func() {}
-	// var platform string
-	// fs.StringVar(&platform, "platform", defaultPlatform, "")
-	// fs.StringVar(&platform, "p", defaultPlatform, "")
 	fs.StringVar(&c.secret, "secret", "", "")
 	fs.StringVar(&c.secret, "s", "", "")
+	var platform string
+	fs.StringVar(&platform, "platform", "", "")
+	fs.StringVar(&platform, "p", "", "")
 	if err = fs.Parse(args); err != nil {
 		return
 	}
-
 	// Save args
-	// switch platform {
-	// case "mac":
-	// 	c.platform = option.QrcodeLoginMac
-	// case "windows":
-	// 	c.platform = option.QrcodeLoginWindows
-	// default:
-	// 	c.platform = option.QrcodeLoginLinux
-	// }
+	c.opts = option.Qrcode()
+	switch platform {
+	case "android":
+		c.opts.LoginAndroid()
+	case "ios":
+		c.opts.LoginIos()
+	case "tv":
+		c.opts.LoginTv()
+	case "wechat":
+		c.opts.LoginWechatMiniApp()
+	case "alipay":
+		c.opts.LoginAlipayMiniApp()
+	case "qandroid":
+		c.opts.LoginQandroid()
+	}
 	if fs.NArg() > 0 {
 		c.saveFile = fs.Arg(0)
 	}
@@ -34,7 +42,7 @@ func (c *Command) Init(args []string) (err error) {
 }
 
 const usageTemplate = `
-Usage: %s [-s secret-key] [save-file]
+Usage: %s [-s secret-key] [-p platform] [save-file]
 
 Description:
     %s
@@ -42,6 +50,10 @@ Description:
 Arguments:
     -s, -secret <secret-key>
         Secret key to encrypt credential, keep it secret!
+	-p, -platform <login-platform>
+	    Simulate login on specific platform.
+		Supported: web/android/ios/tv/wechat/alipay/qandroid
+		Default: web
     save-file
         File to save credential.
 
--- app/login/command.go
+++ app/login/command.go
@@ -1,11 +1,17 @@
 package login
 
+import (
+	"github.com/deadblue/elevengo/option"
+)
+
 const (
 	name = "login"
 	desc = "Simulate 115 client login and export credential."
 )
 
 type Command struct {
+	// Login options
+	opts *option.QrcodeOptions
 	// Secret to encrypt cookie
 	secret string
 	// File to save cookie
